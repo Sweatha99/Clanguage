d02ffc753e2111fe3d9de87f41d29528eed28363
Sweatha99 <68997786+Sweatha99@users.noreply.github.com>
2020-07-30 19:07:14 +0530

    Add files via upload

---
+++ Multiple inputs main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("Enter the first number/n");
+    scanf("%d",&a);
+    printf("Enter the second number/n");
+    scanf("%d",&b);
+    printf("Enter the third number/n");
+    scanf("%d",&c);
+    d=a+b+c;
+    printf("The sum is %d",d);
+    return 0;
+}
+
