f6301f7613986a1038811de58dee805e0c0e293f
Sweatha99 <68997786+Sweatha99@users.noreply.github.com>
2020-07-30 18:21:56 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the first number/n");
+    scanf("%d",&a);
+    printf("Enter the second number/n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum is %d",c);
+
+
+    printf("Enter the first number/n");
+    scanf("%d",&a);
+    printf("Enter the second number/n");
+    scanf("%d",&b);
+    c=a-b;
+    printf("The difference is %d",c);
+
+
+    printf("Enter the first number/n");
+    scanf("%d",&a);
+    printf("Enter the second number/n");
+    scanf("%d",&b);
+    c=a*b;
+    printf("The product is %d",c);
+
+
+    printf("Enter the first number/n");
+    scanf("%d",&a);
+    printf("Enter the second number/n");
+    scanf("%d",&b);
+    c=a/b;
+    printf("The division is %d",c);
+
+
+    printf("Enter the first number/n");
+    scanf("%d",&a);
+    printf("Enter the second number/n");
+    scanf("%d",&b);
+    c=a%b;
+    printf("The percentage is %d",c);
+
+}
