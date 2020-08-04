068b01114cbf20775f0aa5e46ca89482410956a6
Sweatha99 <68997786+Sweatha99@users.noreply.github.com>
2020-08-04 15:17:35 +0530

    Add files via upload

---
+++ Nested-If-Else.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int per;
+    printf("Enter your percentage");
+    scanf("%d",&per);
+    if(per>80)
+    {
+        printf("A grade");
+    }
+    else if(per<80 && per>=70)
+    {
+        printf("B grade");
+    }
+    else if(per<70 && per>=60)
+    {
+        printf("C grade");
+    }
+    else if(per<60 && per>=45)
+    {
+        printf("D grade");
+    }
+    else if(per<45)
+    {
+        printf("FAIL");
+    }
+}
